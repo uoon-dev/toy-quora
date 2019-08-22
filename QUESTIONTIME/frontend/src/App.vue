80959887f2ed53d26412b4455e8bb99999b35d9c
fox <uoon.dev@gmail.com>
2019-08-22 23:29:00 +0900

    connect django and vue

---
+++ QUESTIONTIME/frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <NavbarComponent />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "./components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavbarComponent
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+.btn:focus {
+  box-shadow: none !important;
+}
+</style>
---
+++ QUESTIONTIME/frontend/src/views/Question.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="single-question mt-2">
+    <div class="container">
+      {{ slug }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Question",
+  props: {
+    slug: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
